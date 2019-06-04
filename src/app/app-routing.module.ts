036d31e8064a9ae9df32660b0bed1eb13858af33
Kevin Masters <gmail@kevinmasters.com>
2019-06-03 20:31:47 -0400

    adds chartjs stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { ContactListComponent } from "./contact-list/contact-list.component";
 import { ContactCreateComponent } from "./contact-create/contact-create.component";
 import { HomeComponent } from "./home/home.component";
 import { ComponentsComponent } from "./components/components.component";
+import { ChartsComponent } from "./charts/charts.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "components", component: ComponentsComponent },
+  { path: "charts", component: ChartsComponent },
   { path: "contact-create", component: ContactCreateComponent },
   { path: "contact-list", component: ContactListComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule } from "@angular/forms";
+import { ChartsModule } from "ng2-charts";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { ContactCreateComponent } from "./contact-create/contact-create.component";
 import { ContactListComponent } from "./contact-list/contact-list.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { ComponentsComponent } from './components/components.component';
+import { ComponentsComponent } from "./components/components.component";
+import { ChartsComponent } from "./charts/charts.component";
 
 @NgModule({
   declarations: [
@@ -20,9 +22,16 @@ import { ComponentsComponent } from './components/components.component';
     ContactListComponent,
     HeaderComponent,
     FooterComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    ChartsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    ChartsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
